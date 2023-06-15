be76c2bbede7a0e3b42cfa462fd1e22c4505c0c4
Rudianto <arudi0636@gmail.com>
2023-06-15 17:40:25 +0700

    fisrt commit

---
+++ src/element/button/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Button from './index';
+
+test('Should not allowed click button if isDisable is presenting', () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector('span.disabled')).toBeInTheDocument();
+});
+
+// test("Should not allowed click button if isDisabled is present", () => {
+//     const { container } = render(<Button isDisabled></Button>);
+
+//     expect(container.querySelector("span.disabled")).toBeInTheDocument();
+//   });
